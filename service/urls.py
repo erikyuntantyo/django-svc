8fa78d905dcf4428427c1da506168b7cd5392646
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2024-08-25 20:22:21 +0700

    fix urls

--- service/urls.py
+++ service/urls.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from .views import schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Service API",
+        default_version='v1',
+        description="Django Rest Framework example",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="erik.yuntantyo@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('v1/account/', include('account.urls')),
-    path('v1/songs/', include('songs.urls')),
-    path('v1/users/', include('users.urls')),
+    re_path(r'v1/songs/?', include('songs.urls')),
+    re_path(r'v1/users/?', include('users.urls')),
 ]
--- songs/urls.py
+++ songs/urls.py
@@ -4,5 +4,5 @@ from .views import SongDetailView, SongsListView
 
 urlpatterns = [
     path('', SongsListView.as_view(), name='song-list'),
-    path('<int:pk>', SongDetailView.as_view(), name='song-detail'),
+    path('<str:pk>', SongDetailView.as_view(), name='song-detail'),
 ]
